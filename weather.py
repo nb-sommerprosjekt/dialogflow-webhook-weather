8f29d733514f616cd3bb91d597ee1379988e3b66
nb-sommerprosjekt <andre.walsoe@nb.no>
2018-08-28 19:14:38 +0200

    Fjernet redundans

--- weather.py
+++ weather.py
@@ -1,11 +1,11 @@
 from flask import Flask,request,make_response
 import os,json
-import pyowm
+#import pyowm
 import os
 
 app = Flask(__name__)
-owmapikey=os.environ.get('OWMApiKey') #or provide your key here
-owm = pyowm.OWM(owmapikey)
+#owmapikey=os.environ.get('OWMApiKey') #or provide your key here
+#owm = pyowm.OWM(owmapikey)
 
 #geting and sending response to dialogflow
 @app.route('/webhook', methods=['POST'])
@@ -27,28 +27,8 @@ def webhook():
 def processRequest(req):
     
     result = req.get("result")
-    parameters = result.get("parameters")
-    city = parameters.get("geo-city")
-    observation = owm.weather_at_place(city)
-    w = observation.get_weather()
-    latlon_res = observation.get_location()
-    lat=str(latlon_res.get_lat())
-    lon=str(latlon_res.get_lon())
-     
-    wind_res=w.get_wind()
-    wind_speed=str(wind_res.get('speed'))
-    
-    humidity=str(w.get_humidity())
-    
-    celsius_result=w.get_temperature('celsius')
-    temp_min_celsius=str(celsius_result.get('temp_min'))
-    temp_max_celsius=str(celsius_result.get('temp_max'))
-    
-    fahrenheit_result=w.get_temperature('fahrenheit')
-    temp_min_fahrenheit=str(fahrenheit_result.get('temp_min'))
-    temp_max_fahrenheit=str(fahrenheit_result.get('temp_max'))
-    speech = "Today the weather in " + city + ": \n" + "Temperature in Celsius:\nMax temp :"+temp_max_celsius+".\nMin Temp :"+temp_min_celsius+".\nTemperature in Fahrenheit:\nMax temp :"+temp_max_fahrenheit+".\nMin Temp :"+temp_min_fahrenheit+".\nHumidity :"+humidity+".\nWind Speed :"+wind_speed+"\nLatitude :"+lat+".\n  Longitude :"+lon
-    
+
+    speech = "Dette fungerer. Jeg er Nancy og jeg lever"    
     return {
         "speech": speech,
         "displayText": speech,
